f84d5b4e0af52cebf5e01822a605af7e88c0c9b7
yuzarsif3016 <yusufpathan3016@gmail.com>
2025-06-15 14:24:16 +0530

    1-starting out with basics

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {id:"heading"}, "Hello from React  CDN 🎺");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+console.log(heading);
+
