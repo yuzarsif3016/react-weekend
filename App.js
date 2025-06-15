578485072802c1fa484d56e4c38cb84cbdc3f124
yuzarsif3016 <yusufpathan3016@gmail.com>
2025-06-15 21:26:34 +0530

    parcel power explored

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
-const heading = React.createElement("h1", {id:"heading"}, "Hello from React  CDN 🎺");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+
+const heading = React.createElement("h1", {id:"heading"}, "Hello from React 💕");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 console.log(heading);
 
+ 
