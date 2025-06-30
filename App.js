4f9382c815b41f65286a39750a852d03859d196c
yuzarsif3016 <yusufpathan3016@gmail.com>
2025-06-30 11:00:31 +0530

    func component

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 //React element
 const jsxHeading = (
   <h1 id="heading" className="head">
-    Hello From React JSX🚀
+    Hello From React JSX react element🚀
   </h1>
 );
 
@@ -15,22 +15,28 @@ console.log(jsxHeading);
 //1.Functional Component
 //just a normal js function that return a react element,always starts with capital letter
 
-const HeadingComponenet = ()=>{
-    return(
-        <h1 id="heading" className="head">
-            Hello From React Functional Component🚀
-        </h1>
-    )
-}
+const HeadingComponenet = () => {
+  return (
+    <div id="container">
+        {jsxHeading}
+      <h1 id="heading">Hello From React Functional Component 🚀</h1>
+      
+    </div>
+  );
+};
 
-const FooterComponent = ()=>(<div>
-    <h1>Hello From React Functiona Component Footer 🚀</h1>
-</div>)
-
-
+const FooterComponent = () => (
+  <div>
+    {/* <HeadingComponenet /> */}
+    {/* {HeadingComponenet()} */}
+    <HeadingComponenet></HeadingComponenet>
+    <h1>Hello From React Functional Component Footer 🚀</h1>
+  </div>
+);
 
 //2. Class Component --> old way of writing code
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+//root.render(jsxHeading);
+root.render(<FooterComponent />);
